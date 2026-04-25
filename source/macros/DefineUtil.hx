03bc7068bfee537685cfd870f6299d6f769d0ca2
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-04-25 16:52:19 -0500

    Expr type not found

--- source/macros/DefineUtil.hx
+++ source/macros/DefineUtil.hx
@@ -2,12 +2,12 @@ package macros;
 
 class DefineUtil
 {
-	public static macro function getDefine(define:String, ?defaultValue:String):Expr
+	public static macro function getDefine(define:String, ?defaultValue:String)
 	{
 		return macro $v{haxe.macro.Context.definedValue(define) ?? defaultValue ??  null};
 	}
 
-	public static macro function isDefined(define:String):Expr
+	public static macro function isDefined(define:String)
 	{
 		return macro $v{haxe.macro.Context.definedValue(define) != null};
 	}
