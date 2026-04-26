c32d9d81a994e21efd47ad4a9e55949aad86dc18
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-04-26 08:50:29 -0500

    threshold variables

--- source/pitstop/play/score/Score.hx
+++ source/pitstop/play/score/Score.hx
@@ -2,13 +2,19 @@ package pitstop.play.score;
 
 class Score
 {
+	public static final GOOD_THRESHOLD:Float = 0.2;
+
+	public static final BAD_THRESHOLD:Float = 0.75;
+
+	public static final SHIT_THRESHOLD:Float = 0.9;
+
 	public static function grade(noteDiff:Float):RatingClass
 	{
-		if (noteDiff > Conductor.safeZoneOffset * 0.9)
+		if (noteDiff >= Conductor.safeZoneOffset * SHIT_THRESHOLD)
 			return Rating.SHIT;
-		else if (noteDiff > Conductor.safeZoneOffset * 0.75)
+		else if (noteDiff >= Conductor.safeZoneOffset * BAD_THRESHOLD)
 			return Rating.BAD;
-		else if (noteDiff > Conductor.safeZoneOffset * 0.2)
+		else if (noteDiff >= Conductor.safeZoneOffset * GOOD_THRESHOLD)
 			return Rating.GOOD;
 
 		return Rating.SICK;
