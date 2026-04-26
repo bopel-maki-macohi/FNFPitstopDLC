6fc84adbac5555d194116fb2ff03cfd286a71b35
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-04-26 12:43:18 -0500

    4matting

--- source/pitstop/SongList.hx
+++ source/pitstop/SongList.hx
@@ -2,18 +2,14 @@ package pitstop;
 
 class SongList
 {
-
-    public static var SONGLIST:Array<Array<Dynamic>> = [
+	public static var SONGLIST:Array<Array<Dynamic>> = [
 		#if debug
-        ['Test', 0, 'bf'],
-        #end
-        ['Tutorial', 0, 'gf'],
-        #if INCLUDE_WEEK1
-        ['Bopeebo', 1, 'dad'],
-        ['Fresh', 1, 'dad'],
-        ['Dadbattle', 1, 'dad'],
-        #end
-        ['Argue Park', 1, 'dad'],
-    ];
-    
+		['Test', 0, 'bf'],
+		#end
+		['Tutorial', 0, 'gf'],
+		#if INCLUDE_WEEK1
+		['Bopeebo', 1, 'dad'], ['Fresh', 1, 'dad'], ['Dadbattle', 1, 'dad'],
+		#end
+		['Argue Park', 1, 'dad'],
+	];
 }
