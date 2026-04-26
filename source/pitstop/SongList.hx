5bb2aef8f41b082925c48da7668a8c770601f8b4
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-04-26 08:22:27 -0500

    y not lol

--- source/pitstop/SongList.hx
+++ source/pitstop/SongList.hx
@@ -8,6 +8,11 @@ class SongList
         ['Test', 0, 'bf'],
         #end
         ['Tutorial', 0, 'gf'],
+        #if INCLUDE_WEEK1
+        ['Bopeebo', 1, 'dad'],
+        ['Fresh', 1, 'dad'],
+        ['Dadbattle', 1, 'dad'],
+        #end
         ['Argue Park', 1, 'dad'],
     ];
     
