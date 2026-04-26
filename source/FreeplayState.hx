cb3734d7f390aa3abaf6c19ac5d1de75ae6bc5da
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-04-26 09:54:01 -0500

    add the difficulty for erect songs(?) later down the line

--- source/FreeplayState.hx
+++ source/FreeplayState.hx
@@ -248,7 +248,7 @@ class FreeplayState extends MusicBeatState
 		// lerpScore = 0;
 
 		#if PRELOAD_ALL
-		FlxG.sound.playMusic(Paths.inst(songs[curSelected].songName), 0);
+		FlxG.sound.playMusic(Paths.inst(songs[curSelected].songName, PlayState.storyDifficulty), 0);
 		#end
 
 		var bullShit:Int = 0;
